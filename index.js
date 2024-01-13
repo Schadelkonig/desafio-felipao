2df93f485d35aaf4b6d402e79fb43b8b0ce7347c
Schadelkonig <wl.williamlenon@gmail.com>
2024-01-12 22:46:44 -0300

    adicionei texto

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("olá mundo!");
+function classificarNivelHeroi(nome, xp) {
+    let nivel;
+
+    if (xp < 1000) {
+        nivel = "Ferro";
+    } else if (xp <= 2000) {
+        nivel = "Bronze";
+    } else if (xp <= 5000) {
+        nivel = "Prata";
+    } else if (xp <= 7000) {
+        nivel = "Ouro";
+    } else if (xp <= 8000) {
+        nivel = "Platina";
+    } else if (xp <= 9000) {
+        nivel = "Ascendente";
+    } else if (xp <= 10000) {
+        nivel = "Imortal";
+    } else {
+        nivel = "Radiante";
+    }
+
+    console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
+}
+
+const nomeDoHeroi = "Angus";
+const xpDoHeroi = 7500;
+
+classificarNivelHeroi(nomeDoHeroi, xpDoHeroi);
+
